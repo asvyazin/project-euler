887fa732539385d939823097cb96aca2b2d4deab
Alexander Svyazin <asvyazin+github@gmail.com>
2011-06-25 15:21:22 +0600

    problem #14: yet slow

--- haskell/euler14.hs
+++ haskell/euler14.hs
@@ -3,18 +3,10 @@ import Data.List
 next p
   | even p = p `div` 2
   | otherwise = 3 * p + 1
-                        
-collatzChain n =
-  iter [n]
-  where iter a@(1:_) = a
-        iter a@(p:_) = iter $ (next p):a
-                        
-collatzLength' = length . collatzChain
-collatzLength n = 
-  iter n 1
-  where
-    iter 1 p = p
-    iter q p = iter (next q) (p + 1)
+    
+collatzLength 1 = 1
+collatzLength n =
+  1 + (collatzLength $ next n)
 
 main =
   let
